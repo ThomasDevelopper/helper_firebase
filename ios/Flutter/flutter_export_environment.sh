a21161a51712dcbfb48fff4731c8adfa13d8e2a8
= <thom.ferreira99@gmail.com>
2023-04-03 09:23:34 +0200

    refractor: remove platform

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -3,6 +3,7 @@
 export "FLUTTER_ROOT=C:\Users\thoma\Documents\Flutter\SDK\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\thoma\Desktop\Personal Work Flutter\helper_firebase"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
